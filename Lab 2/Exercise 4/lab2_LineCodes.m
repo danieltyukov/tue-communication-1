c7dfcc56c3b297227e38106e0ebfa7de802f5311
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2024-03-19 10:59:37 +0100

    update

--- Lab 2/Exercise 4/lab2_LineCodes.m
+++ Lab 2/Exercise 4/lab2_LineCodes.m
@@ -1,14 +1,17 @@
 %% lab 2 exercise 4
+% channel 1 is a high pass filter - manchester?
+% channel 2
+
 clear all;
 close all;
 clc;
 
 binary = randi([0, 1], 200, 1);                 % a random binary vector is created
 
-T_b = [...];                                   % Choose bit time. The bit time must be smaller than 
+T_b = 1/(1e6);                                   % Choose bit time. The bit time must be smaller than 
                                                 % 1.95 microseconds.
-Fc = ...;                                       %
+Fc = 2e6;                                       %
                                         
-type = '...';                           %Choose type of line coding
+type = 'unipolarNRZ';                           %Choose type of line coding
 
 [encoded, fs] = lineCode(binary, type , T_b); 
