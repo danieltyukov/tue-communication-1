90a4e6e88558c2c9160047b80beba11385d9c5cc
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2025-05-26 14:02:46 +0200

    Refactor Lab 2 Exercise 2 and 3 scripts: enhance signal processing and visualization

--- Lab_2_V2.0/Exercise_2/lab2_ex2.m
+++ Lab_2_V2.0/Exercise_2/lab2_ex2.m
@@ -1,32 +1,55 @@
 %% lab 2_ex2
 clear;
 close all;
-
 load noisySignal.mat;
 
-%Plot the signal before demodulation
-%write your code here:
+% Plot the signal before demodulation
+figure;
+subplot(2,1,1);
+plot(signal);
+title('Time Domain Signal');
+xlabel('Samples');
+ylabel('Amplitude');
 
+subplot(2,1,2);
+periodogram(signal, [], [], R_s, 'centered');
+title('Frequency Domain Signal (Before Demodulation)');
 
 %% Section 2
-
 % Demodulation PAM
+% Define Sampling Vector
+n = (1:1:length(signal))./R_s; % Sample vector for the local oscillator
 
-n = (1:1:length(signal))./R_s;      % sample vector for the local oscillator
-fc = ...;                           % center frequency chosen by you to convert spectrums to baseband
-LO = cos(2*pi*n*fc);                % local oscillator
+% Define Center Frequency
+fc = 500;
 
-% making a filter for signal 3:
+% Create Local Oscillator
+LO = cos(2*pi*n*fc); % Local oscillator for mixing
 
-filt_signal = designfilt(     );
+% Design a Lowpass Filter
+% CORRECTED: Passband frequency must be less than stopband frequency
+filt_signal = designfilt('lowpassfir', ...
+    'PassbandFrequency', 0.25e3, ...    % Changed from 0.5e3
+    'StopbandFrequency', 0.5e3, ...     % Changed from 0.25e3
+    'PassbandRipple', 2, ...
+    'StopbandAttenuation', 60, ...
+    'SampleRate', 10e3, ...
+    'DesignMethod', 'kaiserwin');
 
-s_c = signal.*LO;                % output after local oscillator - converted to baseband
-s_f = filter(filt_signal, s_c);  % output after LPF
+% Demodulate: Mix with Local Oscillator and Filter
+s_c = signal.*LO; % Output after local oscillator - converted to baseband
+s_f = filter(filt_signal, s_c); % Output after LPF
 
-figure
-subplot(3,1,1)
+% Plot PSDs
+figure;
+subplot(3,1,1);
 periodogram(signal, [], [], R_s, 'centered');
-subplot(3,1,2)
+title('Original Signal Spectrum');
+
+subplot(3,1,2);
 periodogram(s_c, [], [], R_s, 'centered');
-subplot(3,1,3)
+title('Signal After Mixing with LO');
+
+subplot(3,1,3);
 periodogram(s_f, [], [], R_s, 'centered');
+title('Signal After Lowpass Filtering');
--- Lab_2_V2.0/Exercise_3/lab2_ex3.m
+++ Lab_2_V2.0/Exercise_3/lab2_ex3.m
@@ -12,31 +12,37 @@ Bitrate = 15*1e+3;
 %% 
 % Convert to baseband
 
-f_1 = ... ; % choose the appropriate frequencies
-f_2 = ... ;
-f_3 = ... ;
+% figure
+% periodogram(sharedChannel,[],[],R_s,'centered')
+
+f_1 = 175e3;     % 0.175 MHz  – smallest-frequency pair
+f_2 = 250e3;     % 0.250 MHz
+f_3 = 550e3;     % 0.550 MHz  – largest-frequency pair
 
 LO_1 = cos(2*pi*f_1*t_1);
 LO_2 = cos(2*pi*f_2*t_1);
 LO_3 = cos(2*pi*f_3*t_1);
 
-q_1 = ... ;
-q_2 = ... ;
-q_3 = ... ;
+q_1 = sharedChannel .* LO_1;
+q_2 = sharedChannel .* LO_2;
+q_3 = sharedChannel .* LO_3;
 
 % Plot the PSDs of the different signals converted to baseband, to see the
 % effect of the LOs and what the cut-off frequencies of your filters 
 % should be:
 
-...
+LPF = designfilt('lowpassfir', 'StopbandFrequency', 0.5e3, ...
+    'PassbandFrequency' , 0.1e3, 'PassbandRipple', 2, ...
+    'StopbandAttenuation', 60, 'SampleRate', 10e3, ...
+    'DesignMethod', 'kaiserwin');
 
 % define the LPF filter by replacing defining the bandwidth for each signal
 % this should result 
 
 
-B1=...;
-B2=...;
-B3=...;
+B1 = 87.5e3;   % 15 kHz
+B2 = 125e3;   % 30 kHz
+B3 = 275e3;   % 15 kHz
 
 LPF1 = designfilt('lowpassfir','PassbandFrequency', B1 ,'StopbandFrequency',3*B1 ,'PassbandRipple',4,'StopbandAttenuation',60,'SampleRate',R_s,'DesignMethod','kaiserwin');
 LPF2 = designfilt('lowpassfir','PassbandFrequency', B2 ,'StopbandFrequency', 3*B2 ,'PassbandRipple',4,'StopbandAttenuation',30,'SampleRate',R_s,'DesignMethod','kaiserwin');
@@ -52,12 +58,30 @@ r_3 = filter(LPF3, q_3) ;
 % effect of the LOs and what the cut-off frequencies of your filters 
 % should be:
 
-....
+figure;
+subplot(3,1,1);
+periodogram(r_1,[],[],R_s,'centered');
+title('PSD of r\_1 (filtered message 1)');
+xlabel('Frequency (Hz)'); ylabel('Power/Frequency (dB/Hz)');
+
+subplot(3,1,2);
+periodogram(r_2,[],[],R_s,'centered');
+title('PSD of r\_2 (filtered message 2)');
+xlabel('Frequency (Hz)'); ylabel('Power/Frequency (dB/Hz)');
+
+subplot(3,1,3);
+periodogram(r_3,[],[],R_s,'centered');
+title('PSD of r\_3 (filtered message 3)');
+xlabel('Frequency (Hz)'); ylabel('Power/Frequency (dB/Hz)');
 
 %% 
 
 %exercise 3.3: file in the linecodes that you found
 
-message_1 = DecodeMessage(r_1, t_1, 1, "");
-message_2 = DecodeMessage(r_2, t_1, 2, "");
-message_3 = DecodeMessage(r_3, t_1, 3, "");
+message_1 = DecodeMessage(r_1, t_1, 1, "unipolarRZ");
+message_2 = DecodeMessage(r_2, t_1, 2, "unipolarNRZ");
+message_3 = DecodeMessage(r_3, t_1, 3, "bipolarRZ");
+
+fprintf('Message 1: %s\n', message_1);
+fprintf('Message 2: %s\n', message_2);
+fprintf('Message 3: %s\n\n', message_3);
